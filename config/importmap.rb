12873483b693490d228f0111cc6c30c90dbc2263
Vladimir Railean <raileanv@gmail.com>
2025-06-11 22:38:05 +0300

    Add importmap support for Stimulus controllers

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+pin_all_from SpreeVetfortExtensionV5::Engine.root.join("app/javascript/spree_vetfort_extension_v5/controllers"),
+             under: "spree_vetfort_extension_v5/controllers",
+             to: "spree_vetfort_extension_v5/controllers"
+
+draw Spree::Core::Engine.root.join("config/importmap.rb")
