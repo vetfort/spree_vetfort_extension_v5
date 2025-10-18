208d40b4092ffb3e2ea3e6abdafaf741047b5e6f
Vladimir Railean <raileanv@gmail.com>
2025-10-18 13:49:15 +0300

    Refactor ProductsControllerDecorator and bump version to 0.5.0.rc2
    - Updated the ProductsControllerDecorator to use the Spree namespace for better organization.
    - Ensured the generate_description method maintains its functionality for generating product descriptions.
    - Bumped version to 0.5.0.rc2 in SpreeVetfortExtensionV5.

--- app/controllers/spree/decorators/admin/products_controller_decorator.rb
+++ app/controllers/spree/decorators/admin/products_controller_decorator.rb
@@ -1,22 +1,24 @@
 # app/controllers/spree/admin/products_controller_decorator.rb
-module Decorators
-  module Admin
-    module ProductsControllerDecorator
-      def generate_description
-        authorize! :update, @product
-        descriptions = OpenAiServices::ProductDescription.new(@product, current_store).call
+module Spree
+  module Decorators
+    module Admin
+      module ProductsControllerDecorator
+        def generate_description
+          authorize! :update, @product
+          descriptions = OpenAiServices::ProductDescription.new(@product, current_store).call
 
-        descriptions.each do |locale, description|
-          key = "description_#{locale}".to_sym
-          @product.update(key => description[:description])
+          descriptions.each do |locale, description|
+            key = "description_#{locale}".to_sym
+            @product.update(key => description[:description])
+          end
+
+          description = descriptions[current_locale.to_sym][:description]
+
+          render json: { success: true, description: }
         end
-
-        description = descriptions[current_locale.to_sym][:description]
-
-        render json: { success: true, description: }
       end
     end
   end
 end
 
-::Spree::Admin::ProductsController.prepend Decorators::Admin::ProductsControllerDecorator
+::Spree::Admin::ProductsController.prepend Spree::Decorators::Admin::ProductsControllerDecorator
