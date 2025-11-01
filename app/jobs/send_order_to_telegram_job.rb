155daf005bfc6868f5ed03e5c875a07e20db797b
Vladimir Railean <raileanv@gmail.com>
2025-11-01 12:55:03 +0200

    Refactor Telegram job to remove environment variable checks and bump version to 0.5.5 in SpreeVetfortExtensionV5

--- app/jobs/send_order_to_telegram_job.rb
+++ app/jobs/send_order_to_telegram_job.rb
@@ -2,14 +2,10 @@ class SendOrderToTelegramJob < ApplicationJob
   queue_as :default
 
   def perform(order_id)
-    return unless ENV["TELEGRAM_BOT_TOKEN"].present? && ENV["TELEGRAM_CHAT_ID"].present?
-
     order = Spree::Order.find(order_id)
     html  = Bots::TelegramOrderFormatter.html(order)
 
-    Timeout.timeout(5) do
-      Bots::VetfortOpsBot.new.call(message: html)
-    end
+    Bots::VetfortOpsBot.new.call(message: html)
   rescue => e
     Rails.logger.error("[Telegram] order=#{order_id} #{e.class}: #{e.message}")
   end
--- lib/spree_vetfort_extension_v5/version.rb
+++ lib/spree_vetfort_extension_v5/version.rb
@@ -1,5 +1,5 @@
 module SpreeVetfortExtensionV5
-  VERSION = '0.5.4'.freeze
+  VERSION = '0.5.5'.freeze
 
   def gem_version
     Gem::Version.new(VERSION)
