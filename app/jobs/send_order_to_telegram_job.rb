c30d5a56aea092ee03062db405e4748ff5e31b28
Vladimir Railean <raileanv@gmail.com>
2025-11-01 13:45:59 +0200

    Update logging in SendOrderToTelegramJob and VetfortOpsBot, bump version to 0.5.6 in SpreeVetfortExtensionV5
    - Removed unnecessary logging statements in SendOrderToTelegramJob and VetfortOpsBot for cleaner output.
    - Updated version to 0.5.6 in SpreeVetfortExtensionV5.

--- app/jobs/send_order_to_telegram_job.rb
+++ app/jobs/send_order_to_telegram_job.rb
@@ -3,11 +3,8 @@ class SendOrderToTelegramJob < ApplicationJob
 
   def perform(order_id)
     order = Spree::Order.find(order_id)
-    Rails.logger.info("[Telegram] order=#{order.number} sending to telegram")
     html  = Bots::TelegramOrderFormatter.html(order)
-    Rails.logger.info("[Telegram] order=#{order.number} html=#{html}")
     Bots::VetfortOpsBot.new.call(message: html)
-    Rails.logger.info("[Telegram] order=#{order.number} sent to telegram")
   rescue => e
     Rails.logger.error("[Telegram] order=#{order_id} #{e.class}: #{e.message}")
     raise e
